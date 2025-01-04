ef2a719246a5b3f5e76435b330d4052b586fbee7
nestor <nestor.lenaerts@gmail.com>
2025-01-04 23:58:41 +0100

    updated de testfile, testmethode toegevoegd die zou mislukken .

--- CarDecisionApp.Tests/Test1.cs
+++ CarDecisionApp.Tests/Test1.cs
@@ -1,11 +1,23 @@
 ﻿namespace CarDecisionApp.Tests
 {
     [TestClass]
-    public sealed class Test1
+    public class CarDecisionModuleTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+            //dit is om een beetje te denken hoe ik mijn code ga schrijven
+            var decisionModule = new CarDecisionModule();
+            var cars = new List<CarModel>
+            {
+                new CarModel { Name = "Model A", Horsepower = 150 },
+                new CarModel { Name = "Model B", Horsepower = 100 }
+            };
+
+            var result = decisionModule.getBestCar(cars, 120);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("Model A", result[0].Name);
         }
     }
 }
