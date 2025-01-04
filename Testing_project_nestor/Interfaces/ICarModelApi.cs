e13f268b4684cf1644c6f29c1ddfcbf545f49635
nestor <nestor.lenaerts@gmail.com>
2025-01-05 00:14:46 +0100

    simpele task in nieuwe interface files voor api aangemaakt.

---
+++ Testing_project_nestor/Interfaces/ICarModelApi.cs
@@ -0,0 +1,14 @@
+﻿using CarDecisionApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDecisionApp.Interfaces
+{
+    public interface ICarModelApi
+    {
+        Task<List<CarModel>> GetAllCarModelsAsync();
+    }
+}
